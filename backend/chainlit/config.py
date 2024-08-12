8bc1c147e571d33894169d49fbbe824ca57e358d
Lorenzo Pinosa <ing@lorenzopinosa.it>
2024-08-12 14:57:28 +0200

    Feedback callback

--- backend/chainlit/config.py
+++ backend/chainlit/config.py
@@ -23,6 +23,7 @@ from pydantic.dataclasses import dataclass
 from starlette.datastructures import Headers
 
 from chainlit.data.base import BaseDataLayer
+from chainlit.types import Feedback
 from chainlit.logger import logger
 from chainlit.translations import lint_translation_json
 from chainlit.version import __version__
@@ -340,6 +341,9 @@ class CodeSettings:
     on_window_message: Optional[Callable[[str], Any]] = None
     author_rename: Optional[Callable[[str], Awaitable[str]]] = None
     data_layer: Optional[Callable[[], BaseDataLayer]] = None
+
+    on_feedback_update: Optional[Callable[[Feedback], Any]] = None
+    on_feedback_delete: Optional[Callable[[str], Any]] = None
 
 
 @dataclass()
