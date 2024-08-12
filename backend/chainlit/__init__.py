4fbba5972b29bd525bf59c8eef85c8e109c27661
Lorenzo Pinosa <ing@lorenzopinosa.it>
2024-08-12 14:57:28 +0200

    Feedback delete callback

--- backend/chainlit/__init__.py
+++ backend/chainlit/__init__.py
@@ -85,6 +85,7 @@ from .callbacks import (
     send_window_message,
     set_chat_profiles,
     set_starters,
+    on_feedback_delete
 )
 
 if TYPE_CHECKING:
@@ -190,6 +191,7 @@ __all__ = [
     "on_chat_resume",
     "on_chat_start",
     "on_feedback",
+    "on_feedback_delete",
     "on_logout",
     "on_mcp_connect",
     "on_mcp_disconnect",
