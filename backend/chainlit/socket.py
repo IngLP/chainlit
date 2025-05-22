d86dc7f405ef58f606eb019897a8e2c95ef439ca
Lorenzo Pinosa <ing@lorenzopinosa.it>
2025-05-22 19:29:28 +0200

    Added logging on disconnection

--- backend/chainlit/socket.py
+++ backend/chainlit/socket.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 from typing import Any, Dict, Literal, Optional, Tuple, Union
 from urllib.parse import unquote
 
@@ -24,7 +25,6 @@ from chainlit.user import PersistedUser, User
 from chainlit.user_session import user_sessions
 
 WSGIEnvironment: TypeAlias = dict[str, Any]
-
 
 def restore_existing_session(sid, session_id, emit_fn, emit_call_fn):
     """Restore a session from the sessionId provided by the client."""
@@ -202,6 +202,7 @@ async def clean_session(sid):
 
 @sio.on("disconnect")  # pyright: ignore [reportOptionalCall]
 async def disconnect(sid):
+    logger.info(f"Disconnecting session {sid}")
     session = WebsocketSession.get(sid)
 
     if not session:
@@ -216,9 +217,11 @@ async def disconnect(sid):
         await persist_user_session(session.thread_id, session.to_persistable())
 
     async def clear(_sid):
+        logger.info(f"Clearing session {_sid}")
         if session := WebsocketSession.get(_sid):
             # Clean up the user session
             if session.id in user_sessions:
+                logger.info(f"Removing user session {session.id}")
                 user_sessions.pop(session.id)
             # Clean up the session
             await session.delete()
@@ -228,6 +231,7 @@ async def disconnect(sid):
     else:
 
         async def clear_on_timeout(_sid):
+            logging.info(f"Scheduled clearing session {_sid} after timeout {config.project.session_timeout}")
             await asyncio.sleep(config.project.session_timeout)
             await clear(_sid)
 
