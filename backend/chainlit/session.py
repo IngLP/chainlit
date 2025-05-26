6fdf281934ad8f189287c6cfaa4fc0b374848c79
Lorenzo Pinosa <ing@lorenzopinosa.it>
2025-05-26 17:36:05 +0200

    Added support for Pydantic models in session serialization

--- backend/chainlit/session.py
+++ backend/chainlit/session.py
@@ -7,6 +7,7 @@ from contextlib import AsyncExitStack
 from typing import TYPE_CHECKING, Any, Callable, Deque, Dict, Literal, Optional, Union
 
 import aiofiles
+from pydantic import BaseModel
 
 from chainlit.logger import logger
 from chainlit.types import FileReference
@@ -22,6 +23,12 @@ ClientType = Literal["webapp", "copilot", "teams", "slack", "discord"]
 
 class JSONEncoderIgnoreNonSerializable(json.JSONEncoder):
     def default(self, o):
+        if isinstance(o, BaseModel):
+            try:
+                return o.model_dump()
+            except AttributeError:
+                # Fallback for Pydantic v1
+                return o.dict()
         try:
             return super().default(o)
         except TypeError:
